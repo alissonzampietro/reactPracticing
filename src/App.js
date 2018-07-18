d0a4e13e5fcb6b9d5c74929f945d0d0987f498e6
Alisson Zampietro <alisson.zampietro@gmail.com>
2018-07-17 23:58:22 -0300

    [FEAT] implementing data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import ContactList from './components/ContactList'
+import Data from './data/data.json'
 
 class App extends React.Component {
+	componentWillMount() {
+		fetch('http://localhost:8080/src/data/data.json').then(data => {
+			console.log(data)
+		})
+	}
 	render() {
 		return (
 			<div>
 				<h1>Contact List</h1>
+				<ContactList list={Data}/>
 			</div>
 		)
 	}
